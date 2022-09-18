ed65f4cd9debc7ba415498dfada022c4dc5f0748
xuyiyunTom <87967175+xuyiyunTom@users.noreply.github.com>
2022-09-18 18:04:46 +0930

    Delete MapGeneric.cpp

--- MapGeneric.cpp
+++
@@ -1,9 +0,0 @@
-#include "MapGeneric.h"
-
-
-vector<int> MapGeneric::map(vector<int> input){
-    for(vector<int>::iterator iter = input.begin(); iter != input.end(); iter++){
-        *iter = f(*iter);
-    }
-    return input;
-}
